4a0fe212bc7406728df38ff58062ea45c3e3ee66
Christopher Mendoza <tiptop1112@gmail.com>
2024-06-26 16:59:10 -0700

    Completed 03_reverseString assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   let splitWord = word.split('');
+   let reversedWord = ''
+   for(i= splitWord.length -1; i>=0; i--) {
+    reversedWord += splitWord[i];
+   }
+   return reversedWord;
 };
 
 // Do not edit below this line
