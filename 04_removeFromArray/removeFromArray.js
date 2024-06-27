7f92adbf04d03b8bd0051ea76b15a70e0bc1d527
Christopher Mendoza <tiptop1112@gmail.com>
2024-06-26 18:31:59 -0700

    Completed 04_removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...moreArgs) {
+    const newArray = []
+    array.forEach((item) => {
+        if(!moreArgs.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
