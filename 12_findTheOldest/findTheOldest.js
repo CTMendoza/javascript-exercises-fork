5a90e9dc1d79659264ff61f83fda79087625665f
Christopher Mendoza <tiptop1112@gmail.com>
2024-07-17 18:28:22 -0700

    Completed 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function (birthDate, deathDate) {
+    if (!deathDate) {
+      deathDate = new Date().getFullYear();
+    }
+    return deathDate - birthDate;
+  };
 
+const findTheOldest = function(people) {
+    return people.reduce((firstPerson, currentPerson) => {
+        const oldestAge = getAge(firstPerson.yearOfBirth, firstPerson.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return currentPerson;
+        } else {
+            return firstPerson;
+        }
+}) 
 };
 
 // Do not edit below this line
