64c2ef2c0d734c696edd367f0c49ff478b460beb
Christopher Mendoza <tiptop1112@gmail.com>
2024-07-17 17:35:11 -0700

    Completed 09_palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+ // Remove non-alphanumeric characters and convert to lowercase
+ str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+ // Compare the string with its reverse
+ let reversedStr = str.split('').reverse().join('');
+ return str === reversedStr;
 };
 
 // Do not edit below this line
